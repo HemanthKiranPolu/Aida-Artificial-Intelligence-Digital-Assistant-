71eff808773dec8212bffd3656bf6455e72afc07
HemanthKiranPolu <hemanthreddypolu@gmail.com>
2025-11-15 14:01:23 -0800

    Initial Commit

---
+++ ASR/ASRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASRApp.swift
+//  ASR
+//
+//  Created by hemanth kiran Polu on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ASRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
